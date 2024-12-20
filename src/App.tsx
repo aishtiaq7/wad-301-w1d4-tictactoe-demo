4a5e8ed971bc476dad24d3b32ff93ad74de6847c
aishtiaq <awshafishtiaque@gmail.com>
2024-12-19 18:19:51 -0800

    cells are reactive

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,49 @@ function Board() {
   const [squares, setSquares] = useState<(string | null)[]>(
     Array(9).fill(null)
   );
-  // console.log(squares);
 
-  function handleClick() {
+  function handleClick(i:number) {
     console.log("clicked");
+
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+
   }
 
   return (
     <>
       <h1>Tic tac toe</h1>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} onSquareClick={handleClick} />
-        <Square value={squares[2]} onSquareClick={handleClick} />
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={handleClick} />
-        <Square value={squares[4]} onSquareClick={handleClick} />
-        <Square value={squares[5]} onSquareClick={handleClick} />
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={handleClick} />
-        <Square value={squares[7]} onSquareClick={handleClick} />
-        <Square value={squares[8]} onSquareClick={handleClick} />
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
       </div>
     </>
   );
 }
 
-function Square(props) {
-  // const [value, setValue] = useState("");
+interface  SquareProps {
+  value: string | null;
+  onSquareClick: () => void;
+}
 
-  return <button className="square" onClick={props.onSquareClick}>{props.value}</button>;
+function Square({ value, onSquareClick }: SquareProps) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Board;
