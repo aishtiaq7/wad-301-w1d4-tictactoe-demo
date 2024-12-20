5561a9c514f7d248a5727049be215594f40f1681
aishtiaq <awshafishtiaque@gmail.com>
2024-12-19 18:02:47 -0800

    click on cells working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
-function App() {
+function Board() {
+  const [squares, setSquares] = useState<(string | null)[]>(
+    Array(9).fill(null)
+  );
+  // console.log(squares);
+
+  function handleClick() {
+    console.log("clicked");
+  }
 
   return (
     <>
       <h1>Tic tac toe</h1>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+function Square(props) {
+  // const [value, setValue] = useState("");
+
+  return <button className="square" onClick={props.onSquareClick}>{props.value}</button>;
+}
+
+export default Board;
