8c94e4c6f717fb904b68a10640a9630a9d257dff
aishtiaq <awshafishtiaque@gmail.com>
2024-12-19 19:01:06 -0800

    before break checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,86 @@ import { useState } from "react";
 import "./App.css";
 
 function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState<(string | null)[]>(
     Array(9).fill(null)
   );
+  // const [status, setStatus] = useState("Next Player :");
 
-  function handleClick(i:number) {
-    console.log("clicked");
+  function calculateWinner(squares: (string | null)[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function handleClick(i: number) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
 
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
-    setSquares(nextSquares);
 
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext((prev) => !prev);
+
+    const winner = calculateWinner(squares);
+    let status: string = `Next player: ${xIsNext ? "X" : "O"}`;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${xIsNext ? "X" : "O"}`;
+    }
   }
 
   return (
     <>
       <h1>Tic tac toe</h1>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
 
-interface  SquareProps {
+interface SquareProps {
   value: string | null;
   onSquareClick: () => void;
 }
